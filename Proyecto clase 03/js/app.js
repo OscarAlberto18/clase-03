b7150bda5b57d5f0ee26a18581d7e1698b0f6d76
Oscar Alberto 18 <ibanez0675@gmail.com>
2023-03-06 20:35:29 -0300

    clase 03

---
+++ Proyecto clase 03/js/app.js
@@ -0,0 +1,12 @@
+var a = 232;
+
+let b = 120; // let arranca...desde ES6
+
+let myDom = document; // acceso global es el html mismo.
+
+console.log(myDom);
+
+let myapp = document.getElementById("myapp");
+
+myapp.innerHTML = "Estoy modificando mi placeholder";
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # clase-03
+
+Ejercicio.
+
+* Volver a crear un repositorio por la clase.
+* Cargar JS con type="module"
+* Modificar el codigo de la clase anterior y en lugar de usar, van a escribir la info del promp 
